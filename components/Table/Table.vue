fc58014081aece9f99b0eecd45f0e6b49ee00aac
Pablo Veiga <vcpablo@MacBook-Pro-de-Pablo.local>
2019-11-12 21:06:43 -0300

    Introduce Table component

---
+++ components/Table/Table.vue
@@ -0,0 +1,25 @@
+<template>
+  <table>
+    <TableRow
+      v-for="(values, index) in items"
+      :key="index"
+      :values="values"
+    />
+  </table>
+</template>
+
+<script>
+import TableRow from '@vcpablo/table-row/TableRow'
+export default {
+  name: 'JTable',
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  components: {
+    TableRow
+  }
+}
+</script>
